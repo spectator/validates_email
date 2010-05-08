8d450ea5855afad35c70d5876baf0588cb64044a
Yury Velikanau <yury.spectator@gmail.com>
2010-05-08 20:53:40 +0300

    Initial commit.

---
+++ lib/validates_email.rb
@@ -0,0 +1,26 @@
+class EmailValidator < ActiveModel::EachValidator
+
+  LocalPartSpecialChars = Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
+  LocalPartUnquoted = '(([[:alnum:]' + LocalPartSpecialChars + ']+[\.\+]+))*[[:alnum:]' + LocalPartSpecialChars + '+]+'
+  LocalPartQuoted = '\"(([[:alnum:]' + LocalPartSpecialChars + '\.\+]*|(\\\\[\x00-\xFF]))*)\"'
+  Regex = Regexp.new('^((' + LocalPartUnquoted + ')|(' + LocalPartQuoted + ')+)@(((\w+\-+[^_])|(\w+\.[^_]))*([a-z0-9-]{1,63})\.[a-z]{2,6}$)', Regexp::EXTENDED | Regexp::IGNORECASE, 'n')
+
+  def validates_email_format(email)
+    # local part max is 64 chars, domain part max is 255 chars
+    # TODO: should this decode escaped entities before counting?
+    begin
+      domain, local = email.reverse.split('@', 2)
+    rescue
+      false
+    end
+
+    email =~ Regex and not email =~ /\.\./ and domain.length <= 255 and local.length <= 64
+  end
+
+  def validate_each(record, attribute, value)
+    unless validates_email_format(value)
+      record.errors[attribute] << (options[:message] || I18n.t(:invalid, :scope => [:activerecord, :errors, :messages]))
+    end
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,14 @@
+ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
+
+require 'test/unit'
+require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
+
+def load_schema
+  config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
+  ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
+
+  ActiveRecord::Base.establish_connection(config['plugin_test'])
+  load(File.dirname(__FILE__) + "/schema.rb")
+  require File.dirname(__FILE__) + '/../init.rb'
+end
